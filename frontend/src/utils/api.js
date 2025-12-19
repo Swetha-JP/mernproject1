1ae71c132bfed5554ba6c5bc52ce93416e14a0f6
Swetha <jpswetha99@gmail.com>
2025-12-19 14:57:48 +0530

    Fix API URL to include /api path

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { storage } from './helpers';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL 
+  ? `${process.env.REACT_APP_API_URL}/api` 
+  : 'http://localhost:3001/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
